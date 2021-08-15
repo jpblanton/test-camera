3a3d0ccf3c95443da797e6344b6beb0306a20fde
James Blanton <jamespblanton@gmail.com>
2021-08-14 21:59:14 -0400

    init

---
+++ capture_img_paho.py
@@ -0,0 +1,29 @@
+import time
+import io
+
+import picamera
+import paho.mqtt.client as mqtt
+from decouple import config
+
+def on_connect(client, userdata, flags, rc):
+    client.subscribe(config('SUB_TOPIC')
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+
+client.connect(config('MQTT_HOST'), config('MQTT_PORT'), 60)
+
+client.loop_start()
+
+with picamera.PiCamera() as camera:
+    camera.start_preview()
+    time.sleep(2)
+
+    stream = io.BytesIO()
+    for foo in camera.capture_continuous(stream, 'jpeg'):
+        stream.seek(0)
+        client.publish(config('PUB_TOPIC'), payload=stream.getvalue(), retain=True)
+        stream.seek(0)
+        stream.truncate()
+        time.sleep(30)
